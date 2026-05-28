45747e71d9cd4351410c7a9f0647e417fcc6fc49
blaskoivan <blaskocuenta@gmail.com>
2026-05-28 10:32:24 -0300

    fix(continuar).2

--- Actividad10/Ejercicio1/FormPrincipal.cs
+++ Actividad10/Ejercicio1/FormPrincipal.cs
@@ -1,13 +1,20 @@
+using System.Transactions;
+using Ejemplo.Services;
 using Ejercicio1.Services;
 
 namespace Ejercicio1;
 
 public partial class FormPrincipal : Form
 {
-    NumericService servicio=new NumericService();
+    NumericService servicio = new NumericService();
 
     public FormPrincipal()
     {
+
         InitializeComponent();
     }
+    public btnRegistrar.Click(){
+        int valor=Convert.ToInt32(tbValor.Text);
+        servicio.Registrar(valor);
+        }
 }
